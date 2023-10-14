b9b17eb8a1984b2b2588003a73ab83c71ee8aaa5
heaveless <me@heaveless.com>
2023-10-14 01:55:48 -0500

    :tada: init project

---
+++ app/src/main/java/com/heaveless/leafkeyboard/InputDeviceReceiver.kt
@@ -0,0 +1,10 @@
+package com.heaveless.leafkeyboard
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class InputDeviceReceiver: BroadcastReceiver()  {
+    override fun onReceive(context: Context, intent: Intent) {
+    }
+}
